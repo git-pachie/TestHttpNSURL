3946f63ff70d767883afb73f33d711cf5a2b7b88
archie <archieangeles@gmail.com>
2019-11-16 04:11:21 +0800

    test

--- TestNetwork/ViewController.swift
+++ TestNetwork/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posX.count
-    }
+    } 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
